ba87d445e9490b70c41db972a38fab10cf4102f7
GansKatriel <112304004+GansKatriel@users.noreply.github.com>
2022-09-07 07:42:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['62895604670507', 'Claraa', true], 
-['62895604670507', 'TOLONG JGN SPAM', true] 
+['6282146218274', 'My mastah', true], 
+['6283813417529', 'My botz:3', true] 
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -35,13 +35,13 @@ global.APIKeys = { // APIKey Here
 }
 
 global.linkyt = 'https://youtu.be/RlhkqeUDZrA'
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝐂𝐥𝐚𝐫𝐚𝐚'
+global.WM = 'KhurushiBotz By Riell'
 global.linkgc = 'https://chat.whatsapp.com/Gapunya'
-global.linkig = 'https://www.instagram.com/inizenscuy'
+global.linkig = 'https://www.instagram.com/Riellgans'
 global.deslink = ''
-global.logo = 'https://telegra.ph/file/dffdaf0e8d59ad104e526.jpg'
-global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@inizenscuy'
+global.logo = 'https://telegra.ph/file/2aa124a6bd1fdff4f51d9.jpg'
+global.titlink = 'nyari apa?'
+global.bodlink = '0'
 
 global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
 global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
@@ -52,7 +52,7 @@ global.urlnya = global.linkgc
 
 //============= callButtons ============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
+global.phn = '0'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
